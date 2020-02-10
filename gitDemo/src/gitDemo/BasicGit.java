008dd7e954b95f7eb824fbc7a7c346b37bc677de
vsailu <sailaja.vadlamani@gmail.com>
2020-02-10 14:09:24 -0600

    This is a comment

--- gitDemo/src/gitDemo/BasicGit.java
+++ gitDemo/src/gitDemo/BasicGit.java
@@ -4,7 +4,8 @@ public class BasicGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println(" is it working");
+System.out.println(" is it working; yahoo");
+System.out.println("I am changing this again; you get it git commit");
 	}
 
 }

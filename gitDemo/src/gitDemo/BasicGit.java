7226c35a64e82cabed76676e8d7cb2da007ed707
vsailu <sailaja.vadlamani@gmail.com>
2020-02-10 14:36:35 -0600

    code corrected

--- gitDemo/src/gitDemo/BasicGit.java
+++ gitDemo/src/gitDemo/BasicGit.java
@@ -4,12 +4,13 @@ public class BasicGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println(" is it working; yahoo");
-System.out.println("I am changing this again; you get it git commit");
+       System.out.println(" is it working; yahoo");
+       System.out.println("I am changing this again; you get it git commit");
 // adding more
 //adding
 	}
-	static void Testing1{
+	static void Testing1(){
+		
 		// adding the first method
 	}
 	
